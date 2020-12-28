e96da0c92fbb851eac0fb04e1b2526d6583d8820
Guilherme Knorst Magnago <knorstmagnago@gmail.com>
2020-12-28 19:27:23 +0000

    created professor lectures and updated personal query so my_class works

--- html/sections/personal_query.php
+++ html/sections/personal_query.php
@@ -1,6 +1,11 @@
 <?php
     $stmt = $dbh->prepare(
-    "SELECT person.id as person_ide, person.name as person_name, faculty.name as faculty_name, degree.name as degree_name
+    "SELECT person.id as person_ide,
+          person.name as person_name,
+         faculty.name as faculty_name,
+          degree.name as degree_name,
+    enrollment.course as course_acronym,
+     enrollment.class as class_number
     FROM PERSON
     LEFT JOIN ENROLLMENT ON person.id = enrollment.person_id
     LEFT JOIN STUDENT ON person.id = student.id
